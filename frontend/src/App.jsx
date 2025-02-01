cc2faee2ea0152c7deb548c7e28cdc127214f884
jrppena <jppena@up.edu.ph>
2025-02-01 22:34:22 +0800

    initialize backend and frontend with React, Vite, and Express; add basic structure and configurations

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useState } from 'react'
+
+
+function App() {
+
+  return (
+    <>
+      <div >
+        Hello World
+      </div>
+ 
+    </>
+  )
+}
+
+export default App
