69329121b5397c7f9a3199dbadb03abcf1ede2ed
jrppena <jppena@up.edu.ph>
2025-03-03 06:05:39 +0800

    fix: update axios baseURL to use environment variable for production

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,8 @@
-    import axios from "axios";
+import axios from "axios";
 
-    export const axiosInstance = axios.create({
-        baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api": "/api",
-        withCredentials: true, // This is to allow the frontend to send the cookie to the backend
-    });
-
-
+export const axiosInstance = axios.create({
+    baseURL: import.meta.env.MODE === "development"
+        ? "http://localhost:5001/api"
+        : import.meta.env.VITE_API_BASE_URL, // Replace with your actual backend URL
+    withCredentials: true, // This allows cookies to be sent
+});
