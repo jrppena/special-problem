8df91ec5568284a968b938684fe3cea959abb40e
jrppena <jppena@up.edu.ph>
2025-03-03 04:02:09 +0800

    -added production support and static file serving - update dpackage dependencies and created .gitignore

--- backend/index.js
+++ backend/index.js
@@ -4,12 +4,13 @@ import appRoutes from './routes/routes.js';
 import dotenv from 'dotenv';
 import connectDB from './config/mongodb.js';
 import cookieParser from 'cookie-parser';
+import path from "path";
 
 dotenv.config();
 
 const app = express()
 app.use(express.json());
-
+const __dirname = path.resolve();
 const corsOptions = { 
   origin: "http://localhost:5173",
   credentials: true,
@@ -19,6 +20,13 @@ app.use(cors(corsOptions));
 app.use(cookieParser());
 
 appRoutes(app);
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/dist')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  })
+}
 
 app.listen(process.env.PORT,  () => {
   connectDB();
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
     import axios from "axios";
 
     export const axiosInstance = axios.create({
-        baseURL: "http://localhost:5001",
+        baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001": "/",
         withCredentials: true, // This is to allow the frontend to send the cookie to the backend
     });
 
